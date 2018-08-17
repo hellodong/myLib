4333c8d40177ca1ce9a93e22c27ca9c51501f3ac
purefarmer <purepurefarmer@gmail.com>
2018-08-17 14:21:26 +0800

    feat():添加收到限制的log日志

--- writeLog/src/logFile.c
+++ writeLog/src/logFile.c
@@ -3,8 +3,13 @@
 #include "logFile.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
+
+
+#define LINE_MAXDATA    (128)
 
 
 typedef struct
@@ -12,59 +17,82 @@ typedef struct
 	char path[128];
 	int nowLine;
 	int maxLine;
+    long writeOffset;
 }LogFileCtx_t;
 
 
 static LogFileCtx_t g_ctx;
+static char g_temp[128];
 
-static int findMaxLine(FILE *fd, long *offsetPtr)
+
+static int findNowLine(FILE *fd, long *offsetPtr)
 {
 	char temp[1024];
 	char ch[]=" ";
 	char *rp = NULL;
-	int maxLine = -1, line;
+	int nowLine = 0, line = 0;
 
 	memset(temp, 0x00, sizeof(temp));
+    *offsetPtr = 0;
 	while(fgets(temp, sizeof(temp), fd)){
 		rp = strtok(temp, ch);
 		line = atoi(rp);
 		memset(temp, 0x00, sizeof(temp));
-		if (line > maxLine) {
-			maxLine = line;
+		if (line > nowLine) {
+			nowLine = line;
 			*offsetPtr = ftell(fd);
 		}
 	}
-	return maxLine;
-
+	return nowLine;
 }
 
-
-static int findNowLine(FILE *fd)
+static int judgeOffset(long *offsetPtr, int maxLine)
 {
-	long offset, maxLine;
+    if (*offsetPtr < maxLine * LINE_MAXDATA) {
+        return 0;
+    }
+    *offsetPtr = 0;
+    return 1;
+}
 
-	maxLine = findMaxLine(fd, &offset);
+static char* asciiTime(void)
+{
+    time_t t;
+    struct tm *tmPtr = NULL;
+    int len;
 
-	return maxLine;
+    t = time(NULL);
+    tmPtr = localtime(&t);
+    len = snprintf(g_temp, sizeof(g_temp),"%s", asctime(tmPtr));
+    g_temp[len - 1] = ' ';
+    return g_temp;
+}
+
+static int writeData(FILE *fd, int nowLine,  const char data[])
+{
+    char temp[LINE_MAXDATA];
+    int ret;
+    
+    memset(temp, ' ', sizeof(temp));
+    ret = snprintf(temp, sizeof(temp),"%9d %s %s", nowLine, asciiTime(), data);
+    temp[ret] = ' ';
+    temp[LINE_MAXDATA - 1] = '\n';
+    fwrite(temp, sizeof(temp),1, fd);
+    return ret;
 }
 
 int logFileInit(const char *path, int maxLine)
 {
 	strcpy(g_ctx.path, path);
 	g_ctx.maxLine = maxLine;
-	if (!access(path, F_OK)){
-		FILE *fileFd = NULL;
-		int nowLine;
+	FILE *fileFd = NULL;
 
-		fileFd = fopen(g_ctx.path, "a+");
-		if (NULL == fileFd) {
-			return -1;
-		}
-
-		fclose(fileFd);
-	}else{
-		g_ctx.nowLine = 0;
+	fileFd = fopen(g_ctx.path, "a+");
+	if (NULL == fileFd) {
+		return -1;
 	}
+    g_ctx.nowLine = findNowLine(fileFd, &g_ctx.writeOffset);
+	fclose(fileFd);
 	return 0;
 }
 
@@ -74,12 +102,18 @@ int logFileRecd(const char data[])
 		return -1;
 	}
 	FILE *fileFd = NULL;
+    int ret, retFunc;
 
-	fileFd = fopen(g_ctx.path, "a+");
+	fileFd = fopen(g_ctx.path, "rb+");
 	if (NULL == fileFd) {
 		return 0;
 	}
-
+    rewind(fileFd);
+    retFunc = fseek(fileFd, g_ctx.writeOffset, SEEK_SET);
+    ret = writeData(fileFd, ++g_ctx.nowLine,  data);
+    g_ctx.writeOffset = ftell(fileFd);
+    judgeOffset(&g_ctx.writeOffset,g_ctx.maxLine);
 	fclose(fileFd);
-	return strlen(data);
+	return ret;
 }
+
--- writeLog/src/main.c
+++ writeLog/src/main.c
@@ -1,17 +1,61 @@
 
 
+#include "logFile.h"
+
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 
-#include "writeLog.h"
 
 
-int main (void)
+int test (void)
 {
-    LOG_FATAL("this is a test.\r\n");
-    LOG_ERROR("this is a test.\r\n");
-    LOG_WARN("this is a test.\r\n");
-    LOG_INFO("this is a test.\r\n");
-    LOG_TRACE("this is a test.\r\n");
-    LOG_DEBUG("this is a test.\r\n");
+    FILE *fd;
+
+    fd = fopen("./test.txt", "a+");
+    if (NULL == fd) {
+        fprintf(stderr, "File Open Error\n");
+        return 1;
+    }
+    char temp[256]; 
+    memset(temp, ' ', sizeof(temp));
+    temp[255] = '\n';
+    fprintf(fd, "1234 123213123 313123121");
+    fwrite(temp, sizeof(temp),1, fd);
+    fwrite(temp, sizeof(temp),1, fd);
+    fwrite(temp, sizeof(temp),1, fd);
+    fclose(fd);
+    return 0;
+}
+
+
+int test2 (void)
+{
+    time_t t;
+    struct tm *tmPtr = NULL;
+    char temp[128];
+    int len;
+
+    t = time(NULL);
+    tmPtr = localtime(&t);
+    len = snprintf(temp, sizeof(temp),"%s" ,asctime(tmPtr));
+    temp[len - 1] = ' ';
+    printf("%s", temp);
+
+
+    return 0;
+}
+
+int main(void)
+{
+    int i;
+    logFileInit("./test.txt", 100);
+    for(i = 0;i< 100;i++) {
+        logFileRecd("hello");
+    }
+        logFileRecd("hello");
+        logFileRecd("hello");
+        logFileRecd("hello");
+        logFileRecd("hello");
     return 0;
 }

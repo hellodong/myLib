121639409502e323a337cd25cd55e3803777ca3b
hellodong <hellodong@localhost.cn>
2024-08-23 23:30:08 +0800

    os_adapte: posix d_mutex_new, d_mutex_lock, d_mutex_unlock

--- os_adapte/example/example.cpp
+++ os_adapte/example/example.cpp
@@ -4,10 +4,13 @@
 
 #include "d_time.h"
 #include "d_thread.h"
+#include "d_mutex.h"
 #include "d_semaphore.h"
 
 int g_sem_lock_value = 0;
+int g_mutex_lock_value = 0;
 DSem_t hSem;
+DMutex_t hMutex;
 
 void thread1(DPvoid_t data)
 {
@@ -22,10 +25,27 @@ void thread1(DPvoid_t data)
 	}
 }
 
+void thread2mtx(DPvoid_t data)
+{
+	DMutex_t local_mtx =(DMutex_t )data;
+	std::cout <<"thread mutex start " << std::endl;
+	while(1)
+	{
+	    d_mutex_lock(local_mtx, -1);
+            std::cout << "thread( "<<d_thread_self()<< ")mutex lock value "  << g_mutex_lock_value++ << std::endl;
+	    d_mutex_unlock(local_mtx);
+	    d_thread_sleep(1500);
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	hSem = d_sem_new(1);
+
 	DThread_t thread1id = d_thread_new(NULL, D_THREAD_PRI_IDLE, 256 * 1024, thread1, NULL);
+	hMutex = d_mutex_new();
+	DThread_t thread2id = d_thread_new(NULL, D_THREAD_PRI_IDLE, 128 * 1024, thread2mtx, hMutex);
+	DThread_t thread3id = d_thread_new(NULL, D_THREAD_PRI_IDLE, 128 * 1024, thread2mtx, hMutex);
 
 	while (1)
 	{
--- os_adapte/posix/d_posix_mutex.cpp
+++ os_adapte/posix/d_posix_mutex.cpp
@@ -13,7 +13,7 @@ DMutex_t d_mutex_new(void)
         return pMtx;
     }
 
-    rt = pthread_mutex_init(pMtx, NULL);
+    int rt = pthread_mutex_init(pMtx, NULL);
     if (rt != 0)
     {
         free(pMtx);
@@ -25,13 +25,24 @@ DMutex_t d_mutex_new(void)
 
 int d_mutex_lock(DMutex_t mtx, int timeout)
 {
-
+    return pthread_mutex_lock(mtx);
 }
 
-int d_mutex_trylock(DMutex_t mtx);
+int d_mutex_trylock(DMutex_t mtx)
+{
+    return pthread_mutex_trylock(mtx);
+}
 
-int d_mutex_unlock(DMutex_t mtx);
+int d_mutex_unlock(DMutex_t mtx)
+{
+    return pthread_mutex_unlock(mtx);
+}
 
-int d_mutex_destroy(DMutex_t mtx);
+int d_mutex_destroy(DMutex_t mtx)
+{
+    pthread_mutex_destroy(mtx);
+    free(mtx);
+    return 0;
+}
 
 

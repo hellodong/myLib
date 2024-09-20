a3ae557640bb5b720d1da05db08ba29902546b2f
hellodong <hellodong@localhost.com>
2024-09-20 10:41:35 +0800

    os adapter: paktime turn to time to dump

--- os_adapte/d_time.h
+++ os_adapte/d_time.h
@@ -11,6 +11,15 @@ extern "C" {
 
 #define DTIME_BASED_YEAR	(2020)
 
+typedef struct {
+    uint32_t year;
+    uint8_t month;
+    uint8_t day;
+    uint8_t hour;
+    uint8_t min;
+    uint8_t second;
+}DTIMEStruct_t;
+
 typedef struct
 {
 	uint32_t d_year : 6;
@@ -23,6 +32,13 @@ typedef struct
 
 uint32_t d_time_stampms(void);
 uint32_t d_time_getpaktime(void);
+
+#define D_PAKTIME2TIME(pwd,d) \
+    do {\
+        DTIME_t *ptr=(DTIME_t *)pwd;\
+        (d)->year=(uint32_t)(ptr->d_year + DTIME_BASED_YEAR); (d)->month = (uint8_t)(ptr->d_mon); (d)->day = (uint8_t)(ptr->d_day);\
+        (d)->hour=(uint8_t)(ptr->d_h);(d)->min=(uint8_t)(ptr->d_m);(d)->second=(uint8_t)(ptr->d_s);\
+    }while(0)
 
 #ifdef __cplusplus 
 }
--- os_adapte/example/example2.cpp
+++ os_adapte/example/example2.cpp
@@ -5,6 +5,7 @@
 #include "d_semaphore.h"
 
 #include <iostream>
+#include <iomanip>
 
 DSem_t hSemMain;
 DSem_t hSem1;
@@ -26,8 +27,13 @@ int main(int argc, char *argv[])
     hSem1 = d_sem_new(0);
 	DThread_t thread1id = d_thread_new(NULL, D_THREAD_PRI_IDLE, 256 * 1024, thread1, NULL);
 
-    std::cout << __func__ <<" timestamp: "  << d_time_stampms() << thread1id <<std::endl;
-    
+    std::cout << __func__ <<" timestamp: "  << d_time_stampms() <<" thread id:" <<thread1id <<std::endl;
+
+    DTIMEStruct_t d_time = {0};
+    uint32_t now_d_time = d_time_getpaktime();
+    D_PAKTIME2TIME(&now_d_time,&d_time);
+    std::cout <<d_time.year<<"-"<<std::setw(2)<<std::setfill('0')<<d_time.month + 0<<"-"<< std::setw(2)<<std::setfill('0')<< d_time.day + 0<<" "<< std::setw(2)<<std::setfill('0')<< d_time.hour+ 0<<":" << std::setw(2)<<std::setfill('0')  << d_time.min + 0<<":"<< std::setw(2)<<std::setfill('0')<< d_time.second + 0<<std::endl;
+     
     while(1)
     {
         d_sem_post(hSemMain);
